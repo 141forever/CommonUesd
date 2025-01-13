018b667850f9e6c9f9b9c06c89bdf919bae116f9
Cola Chan <57797863+141forever@users.noreply.github.com>
2025-01-13 12:33:04 +0800

    Update llm_api.py

--- llm_api.py
+++ llm_api.py
@@ -1,3 +1,14 @@
+import pdb
+import sys
+import os
+import time
+import json
+import requests
+import copy
+
+import openai
+from openai import OpenAI
+import anthropic
 
 # official o1
 try:
@@ -13,6 +24,18 @@ try:
     )
 
     text = response.choices[0].message.content
-    return text
+    print(text)
+except Exception as e:
+    print(e)
+
+# official claude
+claude_client = anthropic.Anthropic(api_key='sk-ant-')
+try:
+    response = claude_client.messages.create(
+        model="claude-3-5-sonnet-20241022",
+        max_tokens=1024,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    print(response.content[0].text)
 except Exception as e:
     print(e)
